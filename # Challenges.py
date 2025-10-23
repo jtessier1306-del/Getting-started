52d583732d676a6e070b556b1752b9638e6c4ebd
jtessier1306-del <jtessier1306@gmail.com>
2025-10-23 10:17:35 -0700

    Add files via upload
    Variable challenges for growth.

---
+++ # Challenges.py
@@ -0,0 +1,32 @@
+# Challenges.py 
+# date 10/23/2025
+# favorite_number = 7 
+favorite_color = "orange"
+print ("Hello, my name is Jaden")
+print ("My favorite color is orange")
+favorite_number =7
+print ("My favorite number is 7")
+ 
+ #2 Math Challenege
+X = 700
+Y = 400
+Z = X + Y 
+print (Z =1100) 
+print ("700 + 400 = 1100")
+print ("700 - 400 = 300")
+print ("700 * 400 = 280000")
+print ("700 / 400 = 1.75")
+ 
+ #Challenge 3 String variables
+first = "jaden"
+secoond = "Tessier"
+print("first: jaden" + " " + "second: Tessier")
+print ("jaden" + " " + "Tessier")
+print ("jaden Tessier :)")
+
+# Challenege 4 Update Variables
+age = 25 
+print (" I am 25 Years old")
+age = 26
+print (" Next year I will be 26 years old ")
+# Date 10/23/2025
